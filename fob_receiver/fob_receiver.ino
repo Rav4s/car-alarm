4c2e947cfdd022a7cb199faf2b00e40beebbbfe0
Rav4s <ravidhruvishshah@gmail.com>
2022-08-01 10:28:37 -0500

    remove old sensor and more methods

--- fob_receiver/fob_receiver.ino
+++ fob_receiver/fob_receiver.ino
@@ -6,18 +6,10 @@
 // Create Amplitude Shift Keying Object
 RH_ASK rf_driver;
 
-//Initialize pins
-const int trigPin = 3;
-const int echoPin = 2;
-
 void setup()
 {
   // Initialize ASK Object
   rf_driver.init();
-
-  // setup hcsr04 sensor
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
 
   // System is unarmed by default
   boolean armed = false;
@@ -26,23 +18,22 @@ void setup()
   Serial.begin(9600);
 }
 
+void trigRelay() {
+  
+}
+
+void arm() {
+  armed = true;
+  Serial.println("System armed");
+}
+
+void disarm() {
+  armed = false;
+  Serial.println("System disarmed");
+}
+
 void loop()
-{
-  // Clears the trigPin
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  double duration = pulseIn(echoPin, HIGH);
-  // Calculating the distance
-  double distance = duration * 0.034 / 2;
-  // Prints the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.println(distance);
-    
+{   
   // Set buffer to size of expected message
   uint8_t buf[3];
   uint8_t buflen = sizeof(buf);
